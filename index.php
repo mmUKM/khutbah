3233dfa65dcbc8dffc1120d28e20b6312dded918
mmUKM <multimedia@ukm.edu.my>
2019-01-07 10:58:31 +0800

    No commit message

--- index.php
+++ index.php
@@ -87,11 +87,29 @@ get_header();
         </div>
         <div>
             <div class="uk-card-body">
-                <h1>Podcast Terkini</h1>
+                <h1>Terkini</h1>
                 <h3 class="uk-card-title uk-margin-remove"><a href="<?php echo get_post_permalink( $post->ID ); ?>"><?php the_title(); ?></a></h3>
                 <p class="uk-text-meta uk-margin-remove-top"><?php the_field('khutbah_podcast_date'); ?></p>
                 <p class="uk-text-meta uk-text-left"><span uk-icon="user"></span> <?php the_field('khutbah_podcast_khatib'); ?></p>
                 <p class="uk-text-meta uk-text-left uk-margin-remove-top"><span uk-icon="location"></span> <?php the_field('khutbah_podcast_location'); ?></p>
+                <p uk-margin>
+                    <a class="uk-icon-button uk-margin-small-right" uk-icon="soundcloud"
+                        href="
+                        <?php
+                            echo get_post_permalink( $podcast->ID);
+                        ?>
+                        "
+                    >
+                    </a>
+                    <a class="uk-icon-button" uk-icon="youtube"
+                        href="
+                        <?php
+                            echo the_field('khutbah_podcast_video');
+                        ?>
+                        "
+                    >
+                    </a>
+                </p>
             </div>
         </div>
         <?php endwhile; ?>
@@ -138,6 +156,7 @@ get_header();
                         >
                         </a>
                     </p>
+                    <p class="uk-align-center"><?php echo do_shortcode('[Sassy_Social_Share style="background-color:#fff;"]') ?></p>
                 </div>
             </div>
         </div>
